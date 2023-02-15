2b56dd3e2c61b48b0373a974864d928238c903c8
ELCBAh <2611@holbertonstudents.com>
2023-02-15 10:41:52 -0800

    fixed typos

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Description: 'runs program'
  *
- * Returns: sizes
+ * Return: sizes
  */
 int main(void)
 {
@@ -13,19 +13,19 @@ int main(void)
 	 *
 	 * Description: 'sends message to stoud'
 	 *
-	 * Returns: 0 (Success)
+	 * Return: 0 (Success)
 	 */
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %ld byte (s)\n", sizeof(long long int));
-	printf("Size of a float: %ld byte (s)\n", sizeof(float));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	/**
 	 * return - returns 0
 	 *
 	 * Description: 'sends 0'
 	 *
-	 * Returns: 0 (Success)
+	 * Return: 0 (Success)
 	 */
 	return (0);
 }
