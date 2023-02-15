f13d987262ecef0cc1a70a201652a187439e063a
ELCBAh <2611@holbertonstudents.com>
2023-02-15 08:51:48 -0800

    fixed betty's requirements

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-	char *str = "Programming is like building a multilingual puzzle";
+	char *str = "Programming is like building a multilingual puzzle ";
 
 	puts(str);
-	return 0;
+	return (0);
 }
