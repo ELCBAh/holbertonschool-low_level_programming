250d3401e0b4ceb33c6e152af75479aa8484ef4f
ELCBAh <2611@holbertonstudents.com>
2023-02-15 09:27:58 -0800

    first try

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: 'Initializes functiion main'
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return(0);
+}
