f4f4abe089e8cd3f229a396e15d95b414f399038
ELCBAh <2611@holbertonstudents.com>
2023-02-15 09:43:22 -0800

    fixing stuff

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 
 /**
- * main : Entry point
+ * main - Entry point
  *
  * Description: 'Initializes functiion main'
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  *
  */
 
 int main(void)
 {
 	/**
+	 * printf - Prints
+	 *
 	 * Description: Prints text in stdout
+	 *
+	 * Return: correct text
 	 */
 	printf("with proper grammar, but the outcome is a piece of art,");
 	
 	return (0);
 	/**
+	 * return - returns 0
+	 *
 	 * Description: returns 0
+	 *
+	 * Return: 0
 	 */
 }
