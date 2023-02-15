32576e95fd72d5054fdd00d759ba68cba010fcd1
ELCBAh <2611@holbertonstudents.com>
2023-02-15 09:33:01 -0800

    fixed commentaries

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 /**
+ * main : Entry point
+ *
  * Description: 'Initializes functiion main'
+ * 
+ * Return : Always 0 (Success)
+ *
  */
 
 int main(void)
 {
+	/* Prints text in stdout */
 	printf("with proper grammar, but the outcome is a piece of art,");
 
 	return(0);
