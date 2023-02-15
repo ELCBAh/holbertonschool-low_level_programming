5ff68a8f01457150999a36f996386028507146a7
ELCBAh <2611@holbertonstudents.com>
2023-02-15 08:57:28 -0800

    comment addded

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(void) /* Initializing code */
+/* Initialize code */
+
+int main(void)
 {
 	/* Outputs to stout */
 	char *str = "Programming is like building a multilingual puzzle ";
