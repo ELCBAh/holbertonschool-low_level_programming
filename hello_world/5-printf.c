58a6a895c583de5ff21c36511a394dfaf43453b6
ELCBAh <2611@holbertonstudents.com>
2023-02-15 09:38:10 -0800

    te odio betty

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,20 +4,20 @@
  * main : Entry point
  *
  * Description: 'Initializes functiion main'
- * 
+ *
  * Return : Always 0 (Success)
  *
  */
 
 int main(void)
 {
-	/*
+	/**
 	 * Description: Prints text in stdout
 	 */
 	printf("with proper grammar, but the outcome is a piece of art,");
-
+	
 	return (0);
-	/*
+	/**
 	 * Description: returns 0
 	 */
 }
