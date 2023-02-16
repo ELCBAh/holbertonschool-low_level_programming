2fd27dbe410f8a9cdfc49d67980762a02f6811bc
ELCBAh <2611@holbertonstudents.com>
2023-02-16 10:26:10 -0800

    first try

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - initializes code
+ * Description: counts in base 10
+ * Return: numbers
+ */
+int main(void)
+{
+	/**int base10[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};*/
+	int count;
+
+	for (count = 0; count < 10; count++)
+		putchar(count + '0');
+		putchar(44);
+		putchar(32);
+	putchar('\n');
+	return (0);
+}
