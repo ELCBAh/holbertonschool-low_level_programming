65d313f9e64ae732a28880eba7a303f6990902ee
ELCBAh <2611@holbertonstudents.com>
2023-02-16 10:14:40 -0800

    first try

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - initializes code
+ * Description: counts in base 10
+ * Return: numbers
+ */
+int main(void)
+{
+	char *base10 = "0123456789abcdef\n";
+	int count;
+
+	for (count = 0; count < 11; count++)
+		putchar(base10[count]);
+	return (0);
+}
