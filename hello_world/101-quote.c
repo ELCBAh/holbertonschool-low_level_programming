3c9ce2d0702c6ed9a7198cd7a93c2b65061024d3
ELCBAh <2611@holbertonstudents.com>
2023-02-15 18:33:07 -0800

    betty let me go

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	char *textVar = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	fprintf(stderr, "%s\n", textVar );
+	fprintf(stderr, "%s\n" , textVar);
 	return (1);
 }
