8e41718611b514c2b377739b3b5114cd532de379
ELCBAh <2611@holbertonstudents.com>
2023-02-15 18:10:52 -0800

    fixing bettys pretentious desires

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - initializes program
  *
  * Description: prints message directly to strerr
  *
- * Return: 1 (Success)*/
-
+ * Return: 1 (Success)
+ */
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
