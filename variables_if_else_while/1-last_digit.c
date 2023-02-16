5a4809648fd60e71d6c427bb32ac1e3a3fcf167d
ELCBAh <2611@holbertonstudents.com>
2023-02-16 07:23:17 -0800

    fixed excersise interpretation error

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,14 +11,17 @@
 int main(void)
 {
 	int n;
+	int num;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	num = (n % 10);
+
 	if (n > 5)
-		printf("Last digit of n is greater than 5");
+		printf("Last digit of %d is %d and is greater than 5", n, num);
 	if (n == 0)
-		printf("Last digit of n is 0");
+		printf("Last digit of %d is %d and is 0", n, num);
 	if (n < 6 && n != 0)
-		printf("Last digit of n is less than 6 and not 0");
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, num);
 	return (0);
 }
