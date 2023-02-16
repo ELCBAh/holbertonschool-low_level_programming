49f7736c6aabec5bcbea9bebb6cfb323c6b26348
ELCBAh <2611@holbertonstudents.com>
2023-02-15 18:09:00 -0800

    101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - initializes program
+ *
+ * Description: prints message directly to strerr
+ *
+ * Return: 1 (Success)*/
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
