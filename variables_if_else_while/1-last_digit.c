ecfcc85204dd1921b53e50c0ea4c257afaa33637
ELCBAh <2611@holbertonstudents.com>
2023-02-16 08:03:54 -0800

    fixed comparison values

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	num = (n % 10);
 
-	if (n > 5)
+	if (num > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, num);
-	if (n == 0)
+	if (num == 0)
 		printf("Last digit of %d is %d and is 0\n", n, num);
-	if (n < 6 && n != 0)
+	if (num < 6 && n != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
 	return (0);
 }
