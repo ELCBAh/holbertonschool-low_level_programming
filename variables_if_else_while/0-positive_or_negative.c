617eaa01d7fe192e9990bc6c8b1194f1755fa6d3
ELCBAh <2611@holbertonstudents.com>
2023-02-16 06:09:25 -0800

    fixed typo

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is positiv\n", n);
+		printf("%d is positive\n", n);
 	if (n < 0)
 		printf("%d is negative\n", n);
 	if (n == 0)
