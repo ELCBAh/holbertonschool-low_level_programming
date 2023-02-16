2d1bbc6e49b1da418cd9b7a0ba35b556313b7ccf
ELCBAh <2611@holbertonstudents.com>
2023-02-16 10:31:18 -0800

    fixed closings

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-	/**int base10[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};*/
+	/*int base10[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};*/
 	int count;
 
 	for (count = 0; count < 10; count++)
+	{
 		putchar(count + '0');
 		putchar(44);
 		putchar(32);
+	}
 	putchar('\n');
 	return (0);
 }
