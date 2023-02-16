ceda6db45d1b3eb187d25bb5dc5d2e684aade31c
ELCBAh <2611@holbertonstudents.com>
2023-02-15 18:21:34 -0800

    added variable to make betty happy

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char *textVar="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fprintf(stderr,"%s",textVar);
 	return (1);
 }
