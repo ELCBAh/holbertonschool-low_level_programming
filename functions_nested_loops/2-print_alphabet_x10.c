1bb5b299870bbd0f4ef0f7b83d93230ba08badd8
ELCBAh <2611@holbertonstudents.com>
2023-02-17 07:17:14 -0800

    fixing compiling errors

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include "2-main.h"
+#include "main.h"
 /**
  * print_alphabetx10 - prints previous code 10x times
  * Description: Prints abc 10x
  * Return: 0
  */
-void print_alphabetx10(void)
+void print_alphabet(void)
 {
 	int count = 0;
 
 	for (count = 0; count < 11; count++)
 	{
-		print_alphabetx10();
+		print_alphabet();
 		{
 			printf("abcdefghijklmnopqrstuvwxyz\n");
 		}
