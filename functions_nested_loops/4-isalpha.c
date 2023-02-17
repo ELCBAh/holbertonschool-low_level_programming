45cd6cca4debe1c6fa21127e0e7228c467071ff9
ELCBAh <2611@holbertonstudents.com>
2023-02-17 09:57:17 -0800

    changed strategy

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -5,15 +5,14 @@
  * Description: checks if input is a letter
  * Return: boolean
  */
+/**
+ * @c: gets abc to check
+ * Description: checks if its a letter
+ * Return: boolean
+ */
 int _isalpha(int c)
 {
 	if (isalpha(c))
-	{
-		_islower(c);
-		{
-			if (islower(c))
-				return (1);
-		}
-	}
+		return (1);
 	return (0);
 }
