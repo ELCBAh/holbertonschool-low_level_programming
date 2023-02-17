44294fff8de46c2bf55bc18c21c0decf8aa229a9
ELCBAh <2611@holbertonstudents.com>
2023-02-17 11:31:40 -0800

    fixed possible mistakes

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - takes a number
+ * Description: takes a number and prints the last digit
+ * Return: a value
+ * @n: takes a number
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	putchar(abs(last_digit) + '0');
+	return (abs(last_digit) + '0');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,3 +5,4 @@ char _putchar(char r);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
