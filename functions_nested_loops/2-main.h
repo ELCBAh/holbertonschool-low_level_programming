278cc5b3ab4f1a473568bcd8206aeb02ed163732
ELCBAh <2611@holbertonstudents.com>
2023-02-17 07:11:24 -0800

    fixeed bettys styles

--- functions_nested_loops/2-main.h
+++ functions_nested_loops/2-main.h
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * print_alphabet - initializes print alphabet
  * Description: prints alphabet x10 times
  * Return: 0
  */
-void print_alphabet(void)
-	int count =;
-	for(count = 0; count < 11; count++)
-	{
-		print_alphabet();
-	}
+void print_alphabetx10(void);
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "2-main.h"
+/**
+ * print_alphabetx10 - prints previous code 10x times
+ * Description: Prints abc 10x
+ * Return: 0
+ */
+void print_alphabetx10(void)
+{
+	int count = ();
+
+	for (count = 0; count < 11; count++)
+	{
+		printf("abcdefghijklmnopqrstuvwxyz\n");
+	}
+}
