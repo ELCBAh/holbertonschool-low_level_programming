2e9e866f82f831c1c41b135afd4922ac889d337d
ELCBAh <2611@holbertonstudents.com>
2023-02-17 06:57:51 -0800

    repeated last code 10 times

---
+++ functions_nested_loops/2-main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - initializes print alphabet
+ * Description: prints alphabet x10 times
+ * Return: 0
+ */
+void print_alphabet(void)
+	int count =;
+	for(count = 0; count < 11; count++)
+	{
+		print_alphabet();
+	}
