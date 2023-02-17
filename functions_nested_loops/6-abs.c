053d5a80b1117ea20f7b291277f1e02674141924
ELCBAh <2611@holbertonstudents.com>
2023-02-17 10:44:34 -0800

    first try

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - init absolute number
+ * Description: sends to output absolute number
+ * Return: number
+ */
+/**
+ * @int: value to convert to absolute
+ */
+int _abs(int)
+{
+	abs(int);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int _islower(int c);
 char _putchar(char r);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
