00f5aaa28464aea46fc692bbf3067b296d5bc6f2
ELCBAh <2611@holbertonstudents.com>
2023-02-18 09:15:32 -0800

    trying a new approaroach

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+ * jack_bauer - prints 24h
+ * Description: prints 24h
+ * Return: value
+ */
+void jack_bauer(void)
+{
+	int h, m;
+	int i;
+
+	for (i = 0; i < 23; h++)
+	{
+		for (i = 0; i < 59; m++)
+		{
+			while(m < 59)
+			{
+				if(m < 59)
+				{
+					m++;
+					putchar(h / 10 + '0');
+					putchar(h % 10 + '0');
+					putchar(':');
+					putchar(m / 10 + '0');
+					putchar(m % 10 + '0');
+					putchar('\n');
+				}
+			}
+			while(m == 59 && h < 23)
+			{
+				h++;
+				m = 0;
+			}
+		}
+	}
+	return;
+}
