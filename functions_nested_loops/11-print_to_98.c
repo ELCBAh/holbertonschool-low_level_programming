d595ce502037be4f99adb8912362aad93c427e7f
ELCBAh <2611@holbertonstudents.com>
2023-02-19 10:21:12 -0800

    fixed typo

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		{
 			if (n < 10 && n > -10)
 				_putchar(n + '0');
-			else if (n >= 10 | n <= -10)
+			else if (n >= 10 || n <= -10)
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
@@ -37,7 +37,7 @@ void print_to_98(int n)
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 			}
-			else if (n >= 100 | n <= -100)
+			else if (n >= 100 || n <= -100)
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
