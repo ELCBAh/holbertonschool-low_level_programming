bfdd993bd74dd8869db5d1d8d06a2a90d4d5672a
ELCBAh <2611@holbertonstudents.com>
2023-02-19 09:31:34 -0800

    fixed bettys style

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -5,9 +5,15 @@
  * Description: Takes two integers and adds them
  * Return: number
  */
+/**
+ * @a: first number
+ */
+/**
+ * @b: second number
+ */
 int add(int a, int b)
 {
 	int n = a + b;
 
-	return n;
+	return (n);
 }
