770dfbf406be0eeb38ad532ccc6bc3b8f302a8be
ELCBAh <2611@holbertonstudents.com>
2023-02-19 09:29:57 -0800

    finished

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - prototype
+ * Description: Takes two integers and adds them
+ * Return: number
+ */
+int add(int a, int b)
+{
+	int n = a + b;
+
+	return n;
+}
