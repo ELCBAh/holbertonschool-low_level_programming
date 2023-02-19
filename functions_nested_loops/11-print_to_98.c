d3e96f8d0fbc7090d80390107571e0dcbebfb4da
ELCBAh <2611@holbertonstudents.com>
2023-02-19 09:58:53 -0800

    betty

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints from given number to 98
+ * Description: prints to 98
+ */
+/**
+ * @n: takes number
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			if (n < 10 && n > -10)
+				_putchar(n + '0');
+			else if (n >= 10 && <= -10)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		n++;
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n < 100 && n > -100)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else if (n >= 100 && <= -100)
+			{
+				_putchar((n / 100) + '0');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		n--;
+	}
+}
