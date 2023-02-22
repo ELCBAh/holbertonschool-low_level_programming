17d1582a9f586913837a97d68b849799a57b7620
ELCBAh <2611@holbertonstudents.com>
2023-02-22 15:02:11 -0800

    fixed _putchar ammount

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,10 @@ void more_numbers(void)
 	for (i = 0; i <= 10; i++)
 	{
 		for (i = 0; i <= 14; i++)
-			if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-				_putchar(i + '0');
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
