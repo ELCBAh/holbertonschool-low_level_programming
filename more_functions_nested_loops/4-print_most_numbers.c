e0fe04c75637a75dada1ab634d3b376032cc266e
ELCBAh <2611@holbertonstudents.com>
2023-02-22 11:28:13 -0800

    fixed

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,7 @@ void print_most_numbers(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-		if (i == 2 || i == 4)
-		else
-			_putchar(i + '0');
+		if (i != 2 && i != 4)
+			_putchar(i + '0')
 	_putchar('\n');
 }
