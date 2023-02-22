19349fa3c6356447d66877f2eb06fdff973b3055
ELCBAh <2611@holbertonstudents.com>
2023-02-22 15:59:47 -0800

    fixed

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar((i / 10) + '0');
 			if (i >= 10)
-				_putchar((i % 10) + '0');
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
