6576ce331eccd7b90ffd8e93c49ee45a2f089f20
ELCBAh <2611@holbertonstudents.com>
2023-02-22 15:13:58 -0800

    fixed output

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,8 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar((i / 10) + '0');
+			while (i >= 10)
+				_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
