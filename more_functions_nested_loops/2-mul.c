ee1c8838d2c1182647ed8a98fe92f8eb4a334510
ELCBAh <2611@holbertonstudents.com>
2023-02-22 11:09:00 -0800

    first try

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - multiplication}
+ * Description: Multiplies two numbers
+ * Return: Result
+ */
+/**
+ * @a: First value
+ */
+/**
+ * @b: Second value
+ */
+int mul(int a, int b)
+{
+	res = a * b;
+	return (res);
+}
