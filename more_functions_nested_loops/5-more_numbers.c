a718466e5aac6067eaa3145a7c296a4b5dd5248f
ELCBAh <2611@holbertonstudents.com>
2023-02-22 14:53:24 -0800

    first try

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - function to print zero to nine
+ * Description: Prints from zero to nine
+ * Return: Empty
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (i = 0; i <= 14; i++)
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+				_putchar(i + '0');
+		_putchar('\n');
+	}
+}
