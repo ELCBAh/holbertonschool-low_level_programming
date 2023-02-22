fbe420a11e8f0431f46d4fb2fe249be7456e718e
ELCBAh <2611@holbertonstudents.com>
2023-02-22 11:14:37 -0800

    first try

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - function to print zero to nine
+ * Description: Prints from zero to nine
+ * Return: Empty
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(i + '0');
+	_putchar('\n')
+}
