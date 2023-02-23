0a9899315e5d5c8aeefa235b500d29c36d41d757
ELCBAh <2611@holbertonstudents.com>
2023-02-22 17:12:32 -0800

    fixed infinite loop

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,14 @@
 void print_diagonal(int n)
 {
 	int i;
+	int s;
 
 	for (i = 0; i < n; i++)
 	{
 		_putchar(92);
 		if (i > 0)
 		{
-			for (; i <= n;)
+			for (s = 0; s <= i; s++)
 				_putchar(' ');
 		}
 		else
