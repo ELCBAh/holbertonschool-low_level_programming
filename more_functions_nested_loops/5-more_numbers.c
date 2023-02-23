02e3c6faddbfcbfc248b0555b47d3d1d22c9d323
ELCBAh <2611@holbertonstudents.com>
2023-02-22 16:03:25 -0800

    fixed

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,15 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, n;
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (n = 0; n <= 14; n++)
 		{
-			if (i >= 10)
-				_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
