257b10714d724a81c20be0327e08c0131e6ea59c
ELCBAh <2611@holbertonstudents.com>
2023-02-23 09:49:05 -0800

    fixed output spaces

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -20,25 +20,32 @@ void fizz_buzz(void)
 
 	for (i = 1; i < 100; i++)
 	{
-		if (((i % 3) + (i % 5)) == 0) /* filters mul of three and five */
+		if ((i % 3) == 0 && (i % 5) == 0) /* filters mul of three and five */
 			printf("FizzBuzz");
+			putchar(' ');
 		if ((i % 3) == 0) /* filters mul of three */
 			printf("Fizz");
+			putchar(' ');
 		if ((i % 5) == 0) /* filters mul of five */
 			printf("Buzz");
+			putchar(' ');
 		if (i > 99)
 		{
 			putchar((i / 100) + '0');
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
+			putchar(' ');
 		}
 		if (i > 9)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
+			putchar(' ');
 		}
 		else if (i < 10 && i > 0)
+		{
 			putchar((i % 10) + '0');
+			putchar(' ');
+		}
 	}
-		putchar(' ');
 }
