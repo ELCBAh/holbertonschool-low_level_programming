f69aca1079219656f87508cf02a6c933e0bfd64d
ELCBAh <2611@holbertonstudents.com>
2023-02-22 16:26:03 -0800

    first entry

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - Defines protype
+ * Description: Prints diagonal lines
+ * Return: Empty
+ */
+/**
+ * @n: Number of lines
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar("\\");
+		_putchar(' '):
+	}
+}
