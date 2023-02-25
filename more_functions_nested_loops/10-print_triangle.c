808e21548c18912500db65520b5ef8e4614cdcc2
ELCBAh <2611@holbertonstudents.com>
2023-02-24 17:59:31 -0800

    fixed output

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - calls for prototype function
+ * Description: prints triangle
+ * Return - Empty
+ */
+/**
+ * @size: gets triangle size
+ */
+void print_triangle(int size);
+{
+	int i;
+	int qsign;
+
+	if (size > 0)
+	{
+		for (qsign = 1; qsign <= size; qsign++)
+		{
+			for (i = size - qsign; i--)
+				_putchar(' ');
+			for (i = 0; i < qsign; i++)
+				_putchar('#');
+			if (size == qsign)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
