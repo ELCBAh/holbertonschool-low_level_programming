ab543e0e8d7a330332145a2d8af5715ff4dbda63
ELCBAh <2611@holbertonstudents.com>
2023-02-26 18:00:46 -0800

    first entry

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - defines prototype
+ * Description: Prints every number and digits
+ * Return: Void
+ */
+/**
+ * @n: takes number to print
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; n < 1; i++)
+		{
+			if (n < 0)
+				_putchar('-');
+			_putchar(n % 10 + '0');
+			n = n / 10;
+		}
+	}
+	else
+		_putchar('0');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -10,3 +10,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void fizz_buzz(void);
 void print_triangle(int size);
+void print_number(int n);
