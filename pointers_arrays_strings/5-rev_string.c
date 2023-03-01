b17c2cb9c37245028cc3b558b4cd17999a558338
ELCBAh <2611@holbertonstudents.com>
2023-03-01 08:40:44 -0800

    test

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * rev_string - prints str inverted character-wise
+ *
+ * Return: Empty
+ */
+/**
+ * @s: str to print
+ */
+void rev_string(char *s)
+{
+	int i;
+	int maxL = strlen(s) - 1;
+	char *p = &*s;
+
+	while (i < maxL)
+	{
+		*(p + i) = *(p + maxL);
+		i++;
+		maxL--;
+	}
+}
