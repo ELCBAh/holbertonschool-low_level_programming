7903ef31c1e30146e7ccbb74e0c7c7ac24dad605
ELCBAh <2611@holbertonstudents.com>
2023-03-01 11:33:55 -0800

    fixed libraries missing error

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcpy - copy from read only src to dest
+ *
+ * Return: the pointer to dest
+ */
+/**
+ * @dest: where to copy
+ */
+/**
+ * @src: where to get it from
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (strcpy(dest, src));
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void puts2(char *str);
 void _putchar(char c);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
