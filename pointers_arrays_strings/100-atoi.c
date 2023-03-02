10125f3de2819333ab67608c0a862ccdb563e748
ELCBAh <2611@holbertonstudents.com>
2023-03-02 07:58:47 -0800

    fixed segmentation faul error

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _atoi - converts a string to integer
+ *
+ * Return: 1 or 0 in case not int found
+ */
+/**
+ * @s: string to convert
+ */
+int _atoi(char *s)
+{
+	int i;
+	int len = strlen(s);
+	char tmp;
+	
+	for (i = 0; i != '\0'; i++)
+	{
+		if (*s >= '0' && *s <= '9')
+			tmp = s[i];
+		else
+			continue;
+		*s = tmp;
+	}
+}
---
+++ pointers_arrays_strings/test-main.c
@@ -0,0 +1,6 @@
+#include "main.h"
+int main(void)
+{
+	_atoi("402");
+	return (0);
+}
