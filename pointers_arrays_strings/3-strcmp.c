98d4f80935a0c575d3efe60a905f157d5cfaa8a0
ELCBAh <2611@holbertonstudents.com>
2023-03-03 08:57:23 -0800

    fixed not counting string lenght

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -16,7 +16,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0, j = 0; s1[i] != '\0' && s2[j] != '\0'; i++, j++)
 	{
-		if (s1[i] == s2[j])
+		if (s1[i] == s2[j] && strlen(s1) == strlen(s2))
 			return (0);
 		if (strlen(s1) < strlen(s2))
 			return (-1);
