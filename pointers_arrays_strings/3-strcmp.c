ff6ce89e7a7957866a21aa36d554085944299cdd
ELCBAh <2611@holbertonstudents.com>
2023-03-03 08:55:25 -0800

    fixed output

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0, j = 0; s1[i] != '\0' && s2[j] != '\0'; i++, j++)
 	{
-		if (s1[i] == s2[i])
+		if (s1[i] == s2[j])
 			return (0);
 		if (strlen(s1) < strlen(s2))
 			return (-1);
