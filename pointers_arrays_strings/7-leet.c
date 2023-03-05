81690fa0894470ed8a81cb45c154224ba00a61e1
ELCBAh <2611@holbertonstudents.com>
2023-03-05 14:34:24 -0800

    fixed output

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -19,26 +19,6 @@ char *leet(char *a)
 			a[i] = 52;
 			i++;
 		}
-		if (a[i] == 'e' || a[i] == 'E')
-		{
-			a[i] = 51;
-			i++;
-		}
-		if (a[i] == 'o' || a[i] == 'O')
-		{
-			a[i] = 48;
-			i++;
-		}
-		if (a[i] == 't' || a[i] == 'T')
-		{
-			a[i] = 55;
-			i++;
-		}
-		if (a[i] == 'l' || a[i] == 'L')
-		{
-			a[i] = 49;
-			i++;
-		}
 		else
 			continue;
 		i++;
