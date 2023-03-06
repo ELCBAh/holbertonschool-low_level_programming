17a326255f085bc06b237933c9b960b31793ed34
ELCBAh <2611@holbertonstudents.com>
2023-03-06 07:53:04 -0800

    fixed compile error

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - gets length of a prefix substring
+ *
+ * Return: number of bytes in the initial segment of s, consisting only of bytes from accept
+ */
+/**
+ * @s: string
+ */
+/**
+ * @accept: bytes to take
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != ' ' || s[i] != '\0')
+	{
+		if (s[i] == *accept)
+			return (i);
+	}
+	return (i);
+}
