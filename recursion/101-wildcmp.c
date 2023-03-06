52ae39e5dab8755381f0b8954b20c2b5730b4b32
ELCBAh <2611@holbertonstudents.com>
2023-03-06 12:38:19 -0800

    fixed compile error

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -13,10 +13,12 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 != '\0')
 	{
-		if (s1 == s2 && *s1 == '*')
+		if (s1 == s2)
 			return (1);
-		if (s1 != s2)
+		if (s1 != s2 && *s1 == '*')
 			return (0);
+		if (*s1 == '*')
+			return (1);
 	}
 	return (0);
 
