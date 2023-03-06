099f5679593d0e5f1d79704c79310e24fd815440
ELCBAh <2611@holbertonstudents.com>
2023-03-06 12:16:02 -0800

    fixed compile errors

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * is_palindrome - checks string if is a palindrome
+ * Return: 1 if true 0 if false
+ */
+/**
+ * @s: string to check
+ */
+int is_palindrome(char *s)
+{
+	if (*s != '\n')
+	{
+		if (s == s + 1)
+			return (1);
+		else
+			return (0);
+	}
+	return (0);
+}
