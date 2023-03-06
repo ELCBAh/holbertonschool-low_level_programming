1f1e6aedd9062e2f0d7b789c623866940a5f98e8
ELCBAh <2611@holbertonstudents.com>
2023-03-06 09:46:45 -0800

    fixed segmentation error

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - gets length of a string
+ *
+ * Return: length
+ */
+/**
+ * @s: string
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] != '\0')
+	{
+		i++;
+		_strlen_recursion(s + 1);
+	}
+	return (i);
+}
