180654709eec3cfb08688b60ea18a35e2ebb8b61
ELCBAh <2611@holbertonstudents.com>
2023-03-06 12:29:49 -0800

    fixed output

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -13,9 +13,9 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 != '\0')
 	{
-		if (s1 == s2)
+		if (s1 == s2 && s1 == '*')
 			return (1);
-		if (s1 != s2 && *s1 == '*')
+		if (s1 != s2)
 			return (0);
 	}
 	return (0);
