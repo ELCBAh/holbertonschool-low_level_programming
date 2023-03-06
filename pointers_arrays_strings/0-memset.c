087eb9797c22c43177af7c8498dc861586e1c89a
ELCBAh <2611@holbertonstudents.com>
2023-03-06 05:31:34 -0800

    while loop removed

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -18,13 +18,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-	while (*s != '\0')
+	for (i = 0; *s != '\0' && i < n; i++)
 	{
-		for (i = 0;  i < n; i++)
-		{
-			s[i] = b;
-		}
+		s[i] = b;
 	}
 	return (s);
 }
