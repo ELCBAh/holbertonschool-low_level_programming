9a3f9a20613f23e6f65ed36a542304c586b4acdb
ELCBAh <2611@holbertonstudents.com>
2023-03-06 09:26:40 -0800

    fixed putput

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - recurse and reverse a whole string
+ *
+ * Return :Empty
+ */
+/**
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else if (*s == '\0')
+		_putchar('\n');
+}
