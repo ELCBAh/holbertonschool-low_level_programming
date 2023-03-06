2d64a94cb011d23d5e4e679e506738c83b620926
ELCBAh <2611@holbertonstudents.com>
2023-03-06 05:26:32 -0800

    fixed compile error

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include "main.h"
+/**
+ * memset - fills memory with a constant byte
+ *
+ * Return: s pointer
+ */
+/**
+ * @s: bytes of the memory
+ */
+/**
+ * @b: constant byte
+ */
+/**
+ * @n: first n bytes of the memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		for (i = 0;  i < n; i++)
+		{
+			s[i] = b;
+		}
+	}
+	return (s);
+}
