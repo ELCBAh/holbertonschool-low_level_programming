80ae1f802acfd5eedc760db350c0bbbbfb67327b
ELCBAh <2611@holbertonstudents.com>
2023-03-06 11:40:00 -0800

    fixed betty style

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * is_prime_number - checks if number is prime
+ *
+ * Return: 1 if true 0 if false
+ */
+/**
+ * @n: number to check
+ */
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (1);
+	if ((n % n) % 1 == 0)
+		return (1);
+	else
+		return (0);
+}
