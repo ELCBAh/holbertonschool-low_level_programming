0b0c39090aef8f7901e6e863e0a8c3d3df808aab
ELCBAh <2611@holbertonstudents.com>
2023-03-07 10:49:12 -0800

    args.c added

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - initializes funtion
+ * Description: prints numbers of arguments passed
+ * Return: 0
+ */
+/**
+ * @argc: number of arguments
+ */
+/**
+ * @argv: actual array of arguments
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return(0);
+}
