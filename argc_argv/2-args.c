a655e6194f8fc4b2804461b40ab9f0757458dee9
ELCBAh <2611@holbertonstudents.com>
2023-03-07 11:05:34 -0800

    2-args added

---
+++ argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - initialize function
+ * Description: Prints all arguments
+ * Return: 0
+ */
+/**
+ * @argc: number of arguments
+ */
+/**
+ * @argv: array of arguments
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		/* if (i != argc)
+			putchar('\n'); */
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
