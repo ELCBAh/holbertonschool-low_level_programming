63832202cf70f7761a55aa5fb8dca1d5343090ba
ELCBAh <2611@holbertonstudents.com>
2023-03-07 10:38:32 -0800

    fixed bettys requirements

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,6 +4,12 @@
  * Description: A program that prints its own name followed by a new line
  * Return: 0
  */
+/**
+ * @argc: count of array
+ */
+/**
+ * @argv: content of array
+ */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
