9af7a7653a1e314e3851917b97df6bb776d936f5
ELCBAh <2611@holbertonstudents.com>
2023-03-07 10:37:01 -0800

    fixed typo

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * Description: A program that prints its own name followed by a new line
  * Return: 0
  */
-int main(int __atribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
