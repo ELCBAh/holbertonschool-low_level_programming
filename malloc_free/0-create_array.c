a6cdd97bc223c28903263f7a81fc4fb3e9666eeb
ELCBAh <2611@holbertonstudents.com>
2023-03-16 08:06:31 -0700

    fixed memory errors

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - creates array of chars and init specific char
+ * Return: NULL if size 0 or pointer
+ */
+/**
+ * @size: in bytes
+ */
+/**
+ * @c: char to replace
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *b = (char*)malloc(size);
+
+	for (i = 0; i < size; i++)
+		b[i] = c;
+	if (size == 0)
+		return (NULL);
+	return (b);
+}
