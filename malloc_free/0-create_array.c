aab75f355032b5ef06149707b14b04801df3c198
ELCBAh <2611@holbertonstudents.com>
2023-03-16 08:13:21 -0700

    fixing return segmentation fault

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,9 +15,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *b = (char *)malloc(size);
 
-	for (i = 0; i < size; i++)
-		b[i] = c;
 	if (size == 0)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		b[i] = c;
 	return (b);
 }
