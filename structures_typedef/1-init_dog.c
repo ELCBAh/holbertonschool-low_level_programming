0824588cbbe886a6fbe58ec4143ff76230509ce8
ELCBAh <2611@holbertonstudents.com>
2023-03-22 08:27:27 -0700

    fixed return

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,9 +12,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		printf("Ok");
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+
 }
