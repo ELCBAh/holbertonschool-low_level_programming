6f6057f857305428fd8238c86ad2b6c4d14acf6e
ELCBAh <2611@holbertonstudents.com>
2023-03-22 08:05:59 -0700

    fixed output

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <stddef.h>
 #include "dog.h"
 /**
@@ -11,6 +12,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		printf("Ok");
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
