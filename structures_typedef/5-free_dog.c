5369a3765863014b77e64730d5120190f33026d9
ELCBAh <2611@holbertonstudents.com>
2023-03-23 13:32:32 -0700

    added if statement when NULL passed and description

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * free_dog - init prot
+ * @d: structure to free
+ * Description: frees a structure step-by-step
+ */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
 	free(d->name);
 	free(d->owner);
 	free(d);
