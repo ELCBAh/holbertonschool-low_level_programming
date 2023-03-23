917366c7435ccace1f2c1592ab1ed2579e8b1260
ELCBAh <2611@holbertonstudents.com>
2023-03-23 13:29:49 -0700

    removed free age not necessary

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "dog.h"
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,3 +21,4 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 #endif
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
