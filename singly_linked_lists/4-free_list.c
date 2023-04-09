6a9ea33f0f89ed95d70deab24b429e0f920118e2
ELCBAh <2611@holbertonstudents.com>
2023-04-08 18:44:12 -0700

    created temp variable list to iterate through head and using it to free all memmory allocated

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "lists.h"
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
