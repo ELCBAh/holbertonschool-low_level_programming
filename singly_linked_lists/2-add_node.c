e461ecb48fce04741fbb90ee517b0c83b731a54b
ELCBAh <2611@holbertonstudents.com>
2023-04-08 18:19:12 -0700

    fixed names not being counted, defined length

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,6 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
+	new_node->len = strlen(new_node->str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
