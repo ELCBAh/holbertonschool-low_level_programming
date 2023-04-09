9875c823a1681b2c1221642e981988a7dacc6dc0
ELCBAh <2611@holbertonstudents.com>
2023-04-08 18:22:17 -0700

    fixed memmory not being freed after duping string

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,6 +18,11 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->len = strlen(new_node->str);
 	new_node->next = *head;
 	*head = new_node;
