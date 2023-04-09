29989ba864c7dbb1317258f170633cee8f20ca9a
ELCBAh <2611@holbertonstudents.com>
2023-04-08 18:46:02 -0700

    description added

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * free_list - frees allocated memmory in a list
+ * @head: list to be freed
+ * Description: takes a list and frees its allocated memmory
+ */
 void free_list(list_t *head)
 {
 	list_t *temp;
