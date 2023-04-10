ebd54cd5d172eaaa82b1656883d081e4d47e78e7
ELCBAh <2611@holbertonstudents.com>
2023-04-10 09:07:28 -0700

    fixed kernel style description added and added line after variable declaration

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * free_dlistint - initiliazes prototype
+ * @head: list to free
+ * Description: takes a list and frees its allocated memory
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *next_node;
+
 	while (head != NULL)
 	{
 		next_node = head->next;
