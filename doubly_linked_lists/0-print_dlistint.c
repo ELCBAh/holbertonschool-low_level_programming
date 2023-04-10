4cfe17a4158230c73b3f1cb4ac501749d5e4c934
ELCBAh <2611@holbertonstudents.com>
2023-04-10 08:00:57 -0700

    Description added

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_dlistint - initiliazes prototype
+ * @h: list head
+ * Description: prints the contents of a doubly linked list
+ * Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
