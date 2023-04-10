510c7640defd7b17c13c5041aeadc933930537ca
ELCBAh <2611@holbertonstudents.com>
2023-04-10 08:53:54 -0700

    fixed output because last_node was not being checked and implemented correctly, added a definition to move last to previous so it does not overwrite for the next node

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,20 +11,25 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *add_next, *last_node;
 
+	if (head == NULL)
+		return (NULL);
 	add_next = malloc(sizeof(dlistint_t));
 	if (add_next == NULL)
 		return (NULL);
 	add_next->n = n;
+	add_next->next = NULL;
 	if (*head == NULL)
 	{
-		add_next->next = NULL;
+		add_next->prev = NULL;
 		*head = add_next;
-		return (add_next);
 	}
-	last_node = *head;
-	while (last_node->next != NULL)
-		last_node = last_node->next;
-	last_node->next = add_next;
-	add_next->next = NULL;
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = add_next;
+		add_next->prev = last_node;
+	}
 	return (add_next);
 }
