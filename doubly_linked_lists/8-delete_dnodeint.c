3ee19c231bd7ec04310f4dcb887b8d0892ee1084
ELCBAh <2611@holbertonstudents.com>
2023-04-10 18:52:34 -0700

    free function when index = 0 to fix 0 not being properly removed

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -21,6 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = (*head)->next;
 		if (*head != NULL)
 			(*head)->prev = NULL;
+		free(temp);
 		return (1);
 	}
 	temp = *head;
