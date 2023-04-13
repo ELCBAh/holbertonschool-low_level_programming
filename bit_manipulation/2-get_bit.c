992e739caf873f0be6c3be701d8e783d451ad187
ELCBAh <2611@holbertonstudents.com>
2023-04-13 13:08:55 -0700

    checking if index is out of range then, returning the bitmask of the bitwise operation &, and, the previous bitmask that works by not changing the bit at given index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_bit - initializes prototype
+ * @n: given number
+ * @index: index to check number bit
+ * Description: takes a number and checks its bit postition by giben index
+ * Return: bit at index or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bits;
+
+	bits = sizeof(unsigned long int) * 8;
+	if (index >= bits)
+	{
+		return (-1);
+	}
+	return (n & (1ul << index)) >> index;
+}
