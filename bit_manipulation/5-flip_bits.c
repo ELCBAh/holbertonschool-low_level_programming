3935e6081d5ab96c4eee1e043251d2f38e652a7c
ELCBAh <2611@holbertonstudents.com>
2023-04-13 14:11:06 -0700

    checked difference between n and m then counted every 1 encountered

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - initializes prototype
+ * @n: first number to check
+ * @m: second number to check
+ * Description: takes to numbers and checks bits to flip
+ * Return: count of bits*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff;
+	unsigned int count = 0;
+
+	diff = n ^ m;
+	while (diff != 0)
+	{
+		count += diff & 1;
+		diff >>= 1;
+	}
+	return (count);
+}
