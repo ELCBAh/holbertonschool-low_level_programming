2a3b80f66acd52626c7e2b42ef275f0d233afbf0
ELCBAh <2611@holbertonstudents.com>
2023-04-16 09:26:15 -0700

    fixed returning 0 after completion to 1, fixed compile error because of little typo

---
+++ file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * create_file - initializes prototype
+ * @filename: pointer to file
+ * @text_content: is the content of new file
+ * Description: takes a filename and content and creates new file
+ * Return: -1 if fails, 1 if succeeds
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len, f_write;
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		f_write = write(fd, text_content, len);
+		if (f_write != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
