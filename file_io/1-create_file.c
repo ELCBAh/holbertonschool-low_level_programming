7051c36b4b48490c4801d437f17accf76b35807c
ELCBAh <2611@holbertonstudents.com>
2023-04-16 09:35:04 -0700

    fixed betty coding style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,6 +13,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t len, f_write;
+
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
