cddfd03dd1a7b79bcd93233f58ae566af0ef5ea9
ELCBAh <2611@holbertonstudents.com>
2023-04-16 09:58:26 -0700

    checking if permissions are met before appending to fix "should not be able to modify this file" error

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,9 +12,12 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t len, appender;
+	ssize_t len, appender, permission;
 
 	if (filename == NULL)
+		return (-1);
+	permission = access(filename, R_OK | W_OK);
+	if (permission == -1)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND, 0644);
 	if (fd != -1)
