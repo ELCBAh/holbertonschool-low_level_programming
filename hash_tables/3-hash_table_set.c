c2764f92632bdd9db4355aeff66fb8f250cd10b1
ELCBAh <2611@holbertonstudents.com>
2023-04-16 16:52:23 -0700

    fixed typos and betty style errors

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_set - initializes prototype
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ * Description: takes a hash table, finds index and sets a value
+ * Return: 1 in success 0 if fails
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
+}
