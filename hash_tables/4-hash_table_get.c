bcfd471da6d8b294a00c5f890757d2eb244c3a2f
ELCBAh <2611@holbertonstudents.com>
2023-04-16 17:02:42 -0700

    fixed compilation errors, missing stddef.h and passing key not as a const unsigned char *

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - initializes prototype
+ * @ht: hash table
+ * @key: key
+ * Description: find value based on a key
+ * Return: value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
