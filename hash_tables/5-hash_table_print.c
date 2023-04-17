ed4b428c3a01a859bda01dd71677763f67adc03e
ELCBAh <2611@holbertonstudents.com>
2023-04-16 17:52:25 -0700

    temp variable removed, description added and fixed all compilation errors

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_print - initializes prototype
+ * @ht: hash table
+ * Description: prints all contents of a hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+			if (node->next != NULL)
+				printf(", ");
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
