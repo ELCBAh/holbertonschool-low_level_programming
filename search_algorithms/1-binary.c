cbb5c5dea45e52efbddfa63f65c47d6a0ea931ff
ELCBAh <6211@holbertonstudents.com>
2023-05-30 00:35:06 -0300

    binary search function added without functionality for now

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+/**
+ * binary_search - initializes prototype
+ * @array: array to iterate through
+ * @size: size of array
+ * @value: value to search
+ * Description: searches for value in array using binary search
+ * Return: index of value or -1 if not found
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	return (-1);
+}
