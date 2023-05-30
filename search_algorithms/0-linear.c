aeca48ce6372c58a8eaeb0d10c60d44461493146
ELCBAh <6211@holbertonstudents.com>
2023-05-30 00:11:03 -0300

    fixed wrong format in printf

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 
 	while (size--)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i++] == value)
 			return (i - 1);
 	}
